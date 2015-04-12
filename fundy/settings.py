5cdda929cc6468f68ee2e0ffd075785fc4ffe3fc
bryanyen <bryanyen@nonashte-mbp.local>
2015-04-12 08:17:51 -0700

    more ad

--- fundy/settings.py
+++ fundy/settings.py
@@ -221,6 +221,26 @@ SOCIAL_AUTH_PIPELINE = (
 #     'JWT_EXPIRATION_DELTA': datetime.timedelta(days=14)
 # }
 #
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 try:
     from local_settings import *
 except ImportError:
